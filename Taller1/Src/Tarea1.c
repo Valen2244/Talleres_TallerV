a46bca205279c25670bd103903937916ef8df4a1
Valen2244 <vaquinteroo@unal.edu.co>
2022-08-19 15:49:37 -0500

    Cambios entre PC personal y computador del 420.
    Se realizaron ciertos cambios con el propósito de entrenarme más para el cambio de computadores.

--- Taller1/Src/Tarea1.c
+++ Taller1/Src/Tarea1.c
@@ -2,11 +2,14 @@
  * Tarea1.c
  *
  *  Created on: 18/08/2022
- *      Author: if420_01
+ *      Author: Valen2244
  */
 
 
 int main()
 {
-
+	/*
+	 * Hola, estoy tratando de manejar github :)
+	 *
+	 */
 }
